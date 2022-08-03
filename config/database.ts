00a7cfd6b5d3b21005f533c9f8e25f0f2f1a3032
musclednazmul123 <Nazmul@Nazmuls-MacBook-Pro.local>
2022-08-03 14:26:25 -0700

    small updates

--- config/database.ts
+++ config/database.ts
@@ -4,38 +4,38 @@ const { host, port, database, user, password } = parse(process.env.DB_String);
 
 //production
 
-// module.exports = () => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host,
-//       port,
-//       database,
-//       user,
-//       password,
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     },
-//     debug: false,
-//   },
-// });
-
-
-//  development
-
-module.exports = ({ env }) => ({
+module.exports = () => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'Loveinthetimeofcolera1.'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: false,
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     debug: false,
   },
 });
+
+
+//  development
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'postgres'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'Loveinthecolera1.'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: false,
+//     },
+//     debug: false,
+//   },
+// });
