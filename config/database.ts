8dece0ceefdf8c38e65ee62527b43ca6391d1831
BMuscled <bilal.muscled@gmail.com>
2022-11-15 18:05:50 +0500

    proposalForm collection type added

--- config/database.ts
+++ config/database.ts
@@ -25,14 +25,14 @@ module.exports = () => ({
 
 // module.exports = ({ env }) => ({
 //   connection: {
-//     client: "postgres",
+//     client: 'postgres',
 //     connection: {
-//       host: env("DATABASE_HOST", "localhost"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME", "strapi"),
-//       user: env("DATABASE_USERNAME", "postgres"),
-//       password: env("DATABASE_PASSWORD", "Loveinthetimeofcolera1."),
-//       schema: env("DATABASE_SCHEMA", "public"), // Not required
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'postgres'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'bilalmalik10'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
 //       ssl: false,
 //     },
 //     debug: false,
---
+++ src/api/proposal-form/controllers/proposal-form.ts
@@ -0,0 +1,7 @@
+/**
+ *  proposal-form controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::proposal-form.proposal-form');
